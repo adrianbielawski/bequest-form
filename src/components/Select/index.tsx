1a97a2b3c1dad10a8538d5698cb46e1334cd9a4a
Adrian Bielawski <adrian@bielaw.ski>
2021-11-28 11:00:54 +0000

    Required symbol added to select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,21 +4,37 @@ import classNames from 'classnames'
 
 interface Props extends React.InputHTMLAttributes<HTMLSelectElement> {
   className?: string,
+  indicateRequired?: boolean
 }
 
-const Select = React.forwardRef<HTMLSelectElement, Props>(({ className, children, ...rest }, ref) => {
+const Select = React.forwardRef<HTMLSelectElement, Props>(({
+  className,
+  children,
+  required,
+  indicateRequired,
+  ...rest
+}, ref) => {
   const selectClass = classNames(
     'select',
     className,
+    {
+      required: indicateRequired,
+    },
   )
   return (
-    <select
-      className={selectClass}
-      ref={ref}
-      {...rest}
-    >
-      {children}
-    </select>
+    <div className='select-wrapper'>
+      {(required && indicateRequired) && (
+        <span className='required-symbol'>*</span>
+      )}
+      <select
+        className={selectClass}
+        ref={ref}
+        required
+        {...rest}
+      >
+        {children}
+      </select>
+    </div>
   )
 })
 
