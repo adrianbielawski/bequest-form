b4e50e66ee4a5b579710d331e73e2690d7ae60c3
Adrian Bielawski <adrian@bielaw.ski>
2021-11-28 11:00:39 +0000

    Required symbol added to input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,19 +4,34 @@ import classNames from 'classnames'
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string,
+  indicateRequired?: boolean
 }
 
-const Input = React.forwardRef<HTMLInputElement, Props>(({ className, ...rest }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, Props>(({
+  className,
+  required,
+  indicateRequired,
+  ...rest
+}, ref) => {
   const inputClass = classNames(
     'input',
     className,
+    {
+      required: indicateRequired,
+    },
   )
+
   return (
-    <input
-      className={inputClass}
-      ref={ref}
-      {...rest}
-    />
+    <div className='input-wrapper'>
+      {(required && indicateRequired) && (
+        <span className='required-symbol'>*</span>
+      )}
+      <input
+        className={inputClass}
+        ref={ref}
+        {...rest}
+      />
+    </div>
   )
 })
 
